5541fb8ecc9f4dc36773f77087b404d9d124c139
capt-pancakes <don.mccluney@mrg.com>
2020-05-27 17:06:48 -0500

    improved functionality for custom dialog boxes

--- src/bulbox.js
+++ src/bulbox.js
@@ -1,188 +1,237 @@
 /* AUTHOR: capt-pancakes */
-"use strict";
+'use strict'
 const bulbox = {
-    alert: _alert,
-    dialog: _dialog,
-    confirm: _confirm,
-    dismiss: __killDialog,
-    options: {
-        title: undefined,
-        body: undefined,
-        buttons: {
-            confirm: {
-                text: 'Okay',
-                color: 'is-primary',
-                cb: undefined
-            },
-            dismiss: undefined
-        },
-        size: 'lg',
-        bodyIsHtml: false
-    }
-};
+  alert: _alert,
+  dialog: _dialog,
+  confirm: _confirm,
+  dismiss: __killDialog,
+  options: {
+    title: undefined,
+    body: undefined,
+    buttons: {
+      confirm: {
+        text: 'Okay',
+        color: 'is-primary',
+        cb: undefined
+      },
+      dismiss: undefined
+    },
+    size: 'lg',
+    bodyIsHtml: false
+  }
+}
 
 /*
  Each method expects an object that contains a Title, Body, Okay text, Cancel text, and Okay callback and Cancel callback
 */
 
-function _alert(obj) {
-    if (!obj)
-        var obj = {};
+function _alert (obj) {
+  if (!obj) var obj = {}
 
-    if (!obj.title)
-        obj.title = "Alert!";
+  if (!obj.title) obj.title = 'Alert!'
 
-    __buildDialog(obj);
+  __buildDialog(obj)
 }
 
-function _dialog(obj) {
-    if (obj.bodyIsHtml === undefined) obj.bodyIsHtml = true;
-    __buildCustomDialog(obj);
+function _dialog (obj) {
+  if (obj.bodyIsHtml === undefined) obj.bodyIsHtml = true
+  __buildCustomDialog(obj)
 }
 
-function _confirm(obj) {
-    if (obj && !obj.title)
-        obj.title = "Please Confirm";
-    __buildDialog(obj);
+function _confirm (obj) {
+  if (obj && !obj.title) obj.title = 'Please Confirm'
+  __buildDialog(obj)
 }
 
-function __buildDialog({ title, body, buttons, callback, size, bodyIsHtml }) {
-    const parent = document.createElement("div");
-    parent.className = "modal is-active";
-    parent.id = "bulbox-diag-" + __newId();
+function __buildDialog ({ title, body, buttons, callback, size, bodyIsHtml }) {
+  const parent = document.createElement('div')
+  parent.className = 'modal is-active'
+  parent.id = 'bulbox-diag-' + __newId()
 
-    const backdrop = document.createElement("div");
-    backdrop.className = "modal-background appear";
+  const backdrop = document.createElement('div')
+  backdrop.className = 'modal-background appear'
 
-    const content = document.createElement("div");
-    if (size === 'sm') content.style = "width: 400px";
-    content.className = "modal-card appear";
+  const content = document.createElement('div')
+  if (size === 'sm') content.style = 'width: 400px'
+  content.className = 'modal-card appear'
 
+  // Card Header
+  const contentHeader = document.createElement('div')
+  contentHeader.className = 'modal-card-head'
 
-    // Card Header
-    const contentHeader = document.createElement("div");
-    contentHeader.className = "modal-card-head";
+  const contentHeaderTitle = document.createElement('p')
+  contentHeaderTitle.className = 'modal-card-title'
+  contentHeaderTitle.innerText = title
 
-    const contentHeaderTitle = document.createElement("p");
-    contentHeaderTitle.className = "modal-card-title";
-    contentHeaderTitle.innerText = title;
+  const contentHeaderBtn = document.createElement('button')
+  contentHeaderBtn.className = 'delete'
+  contentHeaderBtn.setAttribute(
+    'onclick',
+    `window.bulbox.dismiss('${parent.id}')`
+  )
 
-    const contentHeaderBtn = document.createElement("button");
-    contentHeaderBtn.className = "delete";
-    contentHeaderBtn.setAttribute("onclick", `window.bulbox.dismiss('${parent.id}')`);
+  contentHeader.appendChild(contentHeaderTitle)
+  contentHeader.appendChild(contentHeaderBtn)
 
-    contentHeader.appendChild(contentHeaderTitle);
-    contentHeader.appendChild(contentHeaderBtn);
+  // Card Body
+  const bodyEl = document.createElement('section')
+  if (body) {
+    bodyEl.className = 'modal-card-body'
+    if (bodyIsHtml) {
+      console.log('body is', body)
+      bodyEl.innerHTML = body[0].innerHTML
+    } else {
+      bodyEl.innerText = body
+    }
+  }
 
-    // Card Body
-    const bodyEl = document.createElement("section");
-    if (body) {
-        bodyEl.className = "modal-card-body";
-        if (bodyIsHtml) {
-            console.log("body is", body);
-            bodyEl.innerHTML = body[0].innerHTML;
-        } else {
-            bodyEl.innerText = body;
-        }
+  // Card Footer
+  const footer = document.createElement('footer')
+  footer.className = 'modal-card-foot justify-space-between'
+  if (buttons) {
+    if (buttons.dismiss) {
+      const footerCancelBtn = document.createElement('button')
+      footerCancelBtn.className = 'button ' + buttons.dismiss.color
+      footerCancelBtn.innerText = buttons.dismiss.text
+      footerCancelBtn.addEventListener('click', (el, event) => {
+        window.bulbox.dismiss(parent.id)
+        callback(false)
+      })
+      footer.appendChild(footerCancelBtn)
     }
 
+    if (buttons.confirm) {
+      const footerSaveBtn = document.createElement('button')
+      let localClasses = 'button ' + buttons.confirm.color
+      if (!buttons.dismiss) localClasses += ' is-fullwidth'
+      footerSaveBtn.style = 'margin-left: auto'
+      footerSaveBtn.className = localClasses
+      footerSaveBtn.innerText = buttons.confirm.text
+      footerSaveBtn.addEventListener('click', (el, event) => {
+        window.bulbox.dismiss(parent.id)
+        callback(true)
+      })
+      footer.appendChild(footerSaveBtn)
+    }
+  }
 
+  // Build Content
+  content.appendChild(contentHeader)
+  content.appendChild(bodyEl)
+  content.appendChild(footer)
+
+  // Build Modal
+  parent.appendChild(backdrop)
+  parent.appendChild(content)
+
+  document.body.append(parent)
+}
+
+function __buildCustomDialog ({ body, size, title, buttons, callback }) {
+  const parent = document.createElement('div')
+  parent.className = 'modal is-active'
+  parent.id = 'bulbox-diag-' + __newId()
+
+  const backdrop = document.createElement('div')
+  backdrop.className = 'modal-background appear'
+
+  const content = document.createElement('div')
+  if (size) {
+    if (size === 'sm') content.style = 'width: 400px'
+  }
+  content.className = 'modal-card appear'
+
+  // Card Header
+  const contentHeader = document.createElement('div')
+  contentHeader.className = 'modal-card-head'
+
+  const contentHeaderTitle = document.createElement('p')
+  contentHeaderTitle.className = 'modal-card-title is-size-5'
+  contentHeaderTitle.innerText = title
+
+  const contentHeaderBtn = document.createElement('button')
+  contentHeaderBtn.className = 'delete'
+  contentHeaderBtn.setAttribute(
+    'onclick',
+    `window.bulbox.dismiss('${parent.id}')`
+  )
+
+  contentHeader.appendChild(contentHeaderTitle)
+  contentHeader.appendChild(contentHeaderBtn)
+
+  content.innerHTML = body[0].innerHTML
+  content.prepend(contentHeader)
+
+  if (buttons) {
     // Card Footer
-    const footer = document.createElement("footer");
-    footer.className = "modal-card-foot justify-space-between";
+    const footer = document.createElement('footer')
+    footer.className = 'modal-card-foot justify-space-between'
     if (buttons) {
-        if (buttons.dismiss) {
-            const footerCancelBtn = document.createElement("button");
-            footerCancelBtn.className = "button " + buttons.dismiss.color;
-            footerCancelBtn.innerText = buttons.dismiss.text;
-            footerCancelBtn.addEventListener("click", (el, event) => {
-                window.bulbox.dismiss(parent.id);
-                callback(false);
-            })
-            footer.appendChild(footerCancelBtn);
-        }
+      if (buttons.dismiss) {
+        const footerCancelBtn = document.createElement('button')
+        footerCancelBtn.className = 'button ' + buttons.dismiss.color
+        footerCancelBtn.innerText = buttons.dismiss.text
+        footerCancelBtn.addEventListener('click', (el, event) => {
+          window.bulbox.dismiss(parent.id)
+          if (callback) callback(false)
+        })
+        footer.appendChild(footerCancelBtn)
+      }
 
-        if (buttons.confirm) {
-            const footerSaveBtn = document.createElement("button");
-            let localClasses = "button " + buttons.confirm.color;
-            if (!buttons.dismiss)
-                localClasses += " is-fullwidth";
-            footerSaveBtn.style = "margin-left: auto";
-            footerSaveBtn.className = localClasses;
-            footerSaveBtn.innerText = buttons.confirm.text;
-            footerSaveBtn.addEventListener("click", (el, event) => {
-                window.bulbox.dismiss(parent.id);
-                callback(true);
-            })
-            footer.appendChild(footerSaveBtn);
-        }
-
-
+      if (buttons.confirm) {
+        const footerSaveBtn = document.createElement('button')
+        let localClasses = 'button ' + buttons.confirm.color
+        if (!buttons.dismiss && (buttons.confirm.size  && buttons.confirm.size !== 'sm')) localClasses += ' is-fullwidth'
+        footerSaveBtn.style = 'margin-left: auto'
+        footerSaveBtn.className = localClasses
+        footerSaveBtn.innerText = buttons.confirm.text
+        footerSaveBtn.addEventListener('click', (el, event) => {
+          window.bulbox.dismiss(parent.id)
+          if (callback) callback(true)
+        })
+        footer.appendChild(footerSaveBtn)
+      }
     }
+    content.appendChild(footer)
+  } else {
+    //   content.querySelector('footer').querySelector()
+  }
 
-    // Build Content
-    content.appendChild(contentHeader);
-    content.appendChild(bodyEl);
-    content.appendChild(footer);
-
-    // Build Modal
-    parent.appendChild(backdrop);
-    parent.appendChild(content);
-
-    document.body.append(parent);
+  parent.appendChild(backdrop)
+  parent.appendChild(content)
+  document.body.append(parent)
 }
 
-function __buildCustomDialog({ body, size }) {
-    const parent = document.createElement("div");
-    parent.className = "modal is-active";
-    parent.id = "bulbox-diag-" + __newId();
+function __killDialog (id) {
+  const el = document.querySelector('#' + id)
+  el.firstElementChild.className = 'modal-background disappear'
+  el.lastElementChild.className = 'modal-card disappear'
 
-    const backdrop = document.createElement("div");
-    backdrop.className = "modal-background appear";
-
-    const content = document.createElement("div");
-    if (size) {
-        if (size === 'sm')
-            content.style = "width: 400px";
+  setTimeout(() => {
+    el.parentNode.removeChild(el)
+    bulbox.options = {
+      title: undefined,
+      body: undefined,
+      buttons: {
+        confirm: {
+          text: 'Okay',
+          color: 'is-primary',
+          cb: undefined
+        },
+        dismiss: undefined
+      },
+      size: 'lg',
+      bodyIsHtml: false
     }
-    content.className = "modal-card appear";
-
-    content.innerHTML = body[0].innerHTML;
-    parent.appendChild(content);
-    document.body.append(parent);
+  }, 400)
+  //   bulmabox.params = bulmabox.params.filter((q) => q.id != id);
 }
 
-function __killDialog(id) {
-    const el = document.querySelector('#' + id);
-    el.firstElementChild.className = "modal-background disappear";
-    el.lastElementChild.className = "modal-card disappear";
-
-    setTimeout(() => {
-        el.parentNode.removeChild(el);
-        bulbox.options = {
-            title: undefined,
-            body: undefined,
-            buttons: {
-                confirm: {
-                    text: 'Okay',
-                    color: 'is-primary',
-                    cb: undefined
-                },
-                dismiss: undefined
-            },
-            size: 'lg',
-            bodyIsHtml: false
-        }
-    }, 400);
-    //   bulmabox.params = bulmabox.params.filter((q) => q.id != id);
+window['bulbox'] = bulbox
+function __newId () {
+  return 'xxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 }
-
-
-window["bulbox"] = bulbox;
-function __newId() {
-    return "xxxxxxxx".replace(/[xy]/g, function (c) {
-        var r = (Math.random() * 16) | 0,
-            v = c == "x" ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-    });
-};
--- src/ng-bulbox.js
+++ src/ng-bulbox.js
@@ -1,114 +1,131 @@
-angular.module('ngBulbox', [])
-    .factory('$ngBulbox', ["$q", "$templateCache", "$compile", "$rootScope", "$http", "$window", function ($q, $templateCache, $compile, $rootScope, $http, $window) {
-        return {
-            alert: function (msg) {
-                var deferred = $q.defer();
-                function _callback() {
-                    deferred.resolve();
-                }
-                if (typeof (msg) === "object") {
-                    $window.bulbox.alert(
-                        angular.merge(msg, { callback: _callback })
-                    );
-                }
-                else {
-                    $window.bulbox.alert(msg, _callback);
-                }
-                $compile(document);
-                return deferred.promise;
-            },
-            confirm: function (msg) {
-                var deferred = $q.defer();
-                function _callback(result) {
-                    if (result) {
-                        deferred.resolve();
-                    }
-                    else {
-                        deferred.reject();
-                    }
-                }
-                if (typeof (msg) === "object") {
-                    window.bulbox.confirm(mergeOptions(angular.merge(msg, { callback: _callback })));
-                }
-                else {
-                    window.bulbox.confirm(msg, _callback);
-                }
+angular.module('ngBulbox', []).factory('$ngBulbox', [
+  '$q',
+  '$templateCache',
+  '$compile',
+  '$rootScope',
+  '$http',
+  '$window',
+  function ($q, $templateCache, $compile, $rootScope, $http, $window) {
+    return {
+      alert: function (msg) {
+        var deferred = $q.defer()
+        function _callback () {
+          deferred.resolve()
+        }
+        if (typeof msg === 'object') {
+          $window.bulbox.alert(angular.merge(msg, { callback: _callback }))
+        } else {
+          $window.bulbox.alert(msg, _callback)
+        }
+        $compile(document)
+        return deferred.promise
+      },
+      confirm: function (msg) {
+        var deferred = $q.defer()
+        function _callback (result) {
+          if (result) {
+            deferred.resolve()
+          } else {
+            deferred.reject()
+          }
+        }
+        if (typeof msg === 'object') {
+          window.bulbox.confirm(
+            mergeOptions(angular.merge(msg, { callback: _callback }))
+          )
+        } else {
+          window.bulbox.confirm(msg, _callback)
+        }
 
-                return deferred.promise;
-            },
-            prompt: function (msg, value, selectAllOnFocus) {
-                var deferred = $q.defer();
-                $window.bulbox.prompt({
-                    title: msg,
-                    value: value || '',
-                    selectAllOnFocus: selectAllOnFocus || false,
-                    callback: function (result) {
-                        if (result !== null) {
-                            deferred.resolve(result);
-                        }
-                        else {
-                            deferred.reject();
-                        }
-                    }
-                });
-                return deferred.promise;
-            },
-            customDialog: function (options) {
-                if (options.templateUrl) {
-                    getTemplate(options.templateUrl)
-                        .then(function (template) {
-                            options.scope = options.scope || $rootScope;
-                            options.body = $compile(template)(options.scope);
-                            $window.bulbox.dialog(options);
-                        }, function () { //Show default dialog if no template could be found
-                            $window.bulbox.dialog(options);
-                        });
-                }
-                else {
-                    $window.bulbox.dialog(options);
-                }
-            },
-            setDefaults: function (options) {
-                $window.bulbox.setDefaults(options);
-            },
-            hideAll: function () {
-                $window.bulbox.hideAll();
-            },
-            setLocale: function (name) {
-                $window.bulbox.setLocale(name);
-            },
-            addLocale: function (name, values) {
-                $window.bulbox.addLocale(name, values);
-            },
-            removeLocale: function (name) {
-                $window.bulbox.removeLocale(name);
-            },
-            dismiss: function () {
-                window.bulbox.dismiss();
-            }
-        };
-
-        function getTemplate(templateId) {
-            var def = $q.defer();
-            var template = $templateCache.get(templateId);
-            if (typeof template === "undefined") {
-                $http.get(templateId)
-                    .then(function (response) {
-                        var data = response.data;
-                        $templateCache.put(templateId, data);
-                        def.resolve(data);
-                    });
+        return deferred.promise
+      },
+      prompt: function (msg, value, selectAllOnFocus) {
+        var deferred = $q.defer()
+        $window.bulbox.prompt({
+          title: msg,
+          value: value || '',
+          selectAllOnFocus: selectAllOnFocus || false,
+          callback: function (result) {
+            if (result !== null) {
+              deferred.resolve(result)
             } else {
-                def.resolve(template);
+              deferred.reject()
             }
-            return def.promise;
-        };
+          }
+        })
+        return deferred.promise
+      },
+      customDialog: function (options) {
+        var deferred = $q.defer()
+        function _callback (result) {
+          if (result) {
+            deferred.resolve()
+          } else {
+            deferred.reject()
+          }
+        }
+        if (options.templateUrl) {
+          getTemplate(options.templateUrl).then(
+            function (template) {
+              options.scope = options.scope || $rootScope
+              options.body = $compile(template)(options.scope)
+              $window.bulbox.dialog(
+                angular.merge(options, { callback: _callback })
+              )
+            },
+            function () {
+              //Show default dialog if no template could be found
+              $window.bulbox.dialog(
+                angular.merge(options, { callback: _callback })
+              )
+            }
+          )
+        } else {
+          $window.bulbox.dialog(options)
+        }
+        return deferred.promise
 
-        function mergeOptions(opts) {
-            var buttons = angular.merge($window.bulbox.options.buttons, opts.buttons);
-            var options = angular.merge($window.bulbox.options, opts);
-            options.buttons = buttons;
-            return options;
-        };
+      },
+      setDefaults: function (options) {
+        $window.bulbox.setDefaults(options)
+      },
+      hideAll: function () {
+        $window.bulbox.hideAll()
+      },
+      setLocale: function (name) {
+        $window.bulbox.setLocale(name)
+      },
+      addLocale: function (name, values) {
+        $window.bulbox.addLocale(name, values)
+      },
+      removeLocale: function (name) {
+        $window.bulbox.removeLocale(name)
+      },
+      dismiss: function () {
+        window.bulbox.dismiss()
+      }
+    }
 
-    }]);
+    function getTemplate (templateId) {
+      var def = $q.defer()
+      var template = $templateCache.get(templateId)
+      if (typeof template === 'undefined') {
+        $http.get(templateId).then(function (response) {
+          var data = response.data
+          $templateCache.put(templateId, data)
+          def.resolve(data)
+        })
+      } else {
+        def.resolve(template)
+      }
+      return def.promise
+    }
+
+    function mergeOptions (opts) {
+      var buttons = angular.merge($window.bulbox.options.buttons, opts.buttons)
+      var options = angular.merge($window.bulbox.options, opts)
+      options.buttons = buttons
+      return options
+    }
+  }
+])
